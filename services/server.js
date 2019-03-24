1db31d0c6a481c8653f80b2311967314f9008f35
jxxuxuefeng <jxxuxuefeng@qq.com>
2019-03-24 22:03:56 +0800

    增加更新、导入功能，新增|增加不能有重复i18nKey逻辑

--- services/server.js
+++ services/server.js
@@ -1,69 +1,98 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
+const xlsx = require('node-xlsx');
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart();
 
 const DB_URL = 'mongodb://localhost:27017/tms-intl-library';
 mongoose.connect(DB_URL);
 
 const IntlList = mongoose.model(
-    'intlList' ,
+    'intlList',
     new mongoose.Schema({
         appId: {
             type: String
-        } ,
+        },
         appName: {
             type: String
-        } ,
+        },
         i18nKey: {
-            type: String ,
+            type: String,
             require: true
-        } ,
+        },
         zhCN: {
             type: String
-        } ,
+        },
         enUS: {
             type: String
         }
     })
 );
 
-// User.create({ name: '王五yi', age: 32 });
-
-// IntlList.create({
-//     appId: 'ccp' ,
-//     appName: 'eCooperate™' ,
-//     i18nKey: 'CCP_F_01002',
-//     zhCN: '项目',
-//     enUS: 'Project'
-// });
-
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/intl/add' , (req , res) => {
-    IntlList.create(req.body , (err , doc) => {
-        res.json(doc);
+app.post('/intl/add', (req, res) => {
+    IntlList.findOne({i18nKey: req.body.i18nKey}, (err, doc) => {
+        if (doc) {
+            res.json({success: false, errorMessage: 'i18nKey不能重复'});
+        } else {
+            IntlList.create(req.body, (err, doc) => {
+                res.json({...doc, success: true});
+            })
+        }
+    })
+});
+
+app.get('/intl/list', (req, res) => {
+    IntlList.find({i18nKey: {$regex: req.query.i18nKey}}, (err, doc) => {
+        res.json({data: doc, success: true});
     });
 });
 
-app.get('/intl/list' , (req , res) => {
-    IntlList.find({i18nKey: {$regex: req.query.i18nKey}} , (err , doc) => {
-        res.json(doc);
+app.delete('/intl/delete', (req, res) => {
+    IntlList.remove(req.query, (err, doc) => {
+        res.json({...doc, success: true});
     });
 });
 
-app.delete('/intl/delete',(req , res) => {
-    IntlList.remove(req.query , (err , doc) => {
-        res.json(doc);
+app.put('/intl/updata', (req, res) => {
+    IntlList.findOne({i18nKey:req.body.i18nKey}, (err, doc) => {
+        if(doc){
+            const id = doc._doc._id.toString();
+            if(req.body._id === id){
+                IntlList.findByIdAndUpdate(req.body._id, req.body, (err, doc) => {
+                    res.json({...doc, success: true});
+                })
+            }else{
+                res.json({success: false, errorMessage: 'i18nKey不能重复'});
+            }
+        }else{
+            IntlList.findByIdAndUpdate(req.body._id, req.body, (err, doc) => {
+                res.json({...doc, success: true});
+            })
+        }}
+    )
+});
+
+app.post('/intl/import', multipartMiddleware, (req, res) => {
+    const workSheetsFromFile = xlsx.parse(req.files.file.path);
+    const data = [];
+    workSheetsFromFile[0]['data'].forEach(item => {
+        data.push({
+            appId: 'ccp',
+            appName: 'eCooperate™',
+            i18nKey: item[0],
+            zhCN: item[1],
+            enUS: item[2]
+        })
+    });
+    IntlList.insertMany(data, (err, doc) => {
+        res.json({...doc, success: true});
     });
 });
 
-app.put('/intl/updata' , (req , res) => {
-    IntlList.create(req.body , (err , doc) => {
-        res.json(doc);
-    });
-});
-
-app.listen(9093 , () => {
+app.listen(9093, () => {
     console.log('node app start at port 9093');
 });
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
-import React , { Component } from 'react';
-import { Input , Button , Table , Divider , Row , Col , Popconfirm } from 'antd';
+import React, {Component} from 'react';
+import {Input, Button, Table, Divider, Row, Col, Popconfirm, Upload, message} from 'antd';
 import axios from 'axios';
 
 const Search = Input.Search;
@@ -11,114 +11,118 @@ class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dataList: [] ,
-            loading: false ,
-            visible: false ,
+            dataList: [],
+            loading: false,
+            visible: false,
             rowData: null
         };
-        this.colums = [ {
-            title: '应用名称' ,
-            dataIndex: 'appName' ,
-            key: 'appName' ,
+        this.colums = [{
+            title: '应用名称',
+            dataIndex: 'appName',
+            key: 'appName',
             // width: 100
-        } , {
-            title: 'i18nKey' ,
-            dataIndex: 'i18nKey' ,
-            key: 'i18nKey' ,
+        }, {
+            title: 'i18nKey',
+            dataIndex: 'i18nKey',
+            key: 'i18nKey',
             // width: 100
-        } , {
-            title: '中文' ,
-            dataIndex: 'zhCN' ,
-            key: 'zhCN' ,
+        }, {
+            title: '中文',
+            dataIndex: 'zhCN',
+            key: 'zhCN',
             // width: 100,
-            render: (text , record) => {
+            render: (text, record) => {
                 return text;
             }
-        } , {
-            title: '英文' ,
-            dataIndex: 'enUS' ,
-            key: 'enUS' ,
-            render: (text , record) => {
+        }, {
+            title: '英文',
+            dataIndex: 'enUS',
+            key: 'enUS',
+            render: (text, record) => {
                 return text;
             }
-        } , {
-            title: '操作' ,
-            dataIndex: 'action' ,
-            key: 'action' ,
-            width: 120 ,
-            render: (text , record) => {
+        }, {
+            title: '操作',
+            dataIndex: 'action',
+            key: 'action',
+            width: 120,
+            render: (text, record) => {
                 return (
                     <span>
                       <a href="javascript:void(0);" onClick={() => this.onEdit(record)}>修改</a>
-                      <Divider type="vertical" />
+                      <Divider type="vertical"/>
                          <Popconfirm title="确定要删除该记录嘛？" onConfirm={() => this.onDelete(record._id)}>
                              <a href="javascript:void(0);">删除</a>
                          </Popconfirm>
                     </span>
                 );
             }
-        } ];
+        }];
     }
-    
-    
+
+
     componentDidMount() {
         this.getList();
     }
-    
+
     // 查询
     onSearch = (value) => {
         this.getList(value);
     };
-    
+
     // 新增
     onAdd = () => {
         this.setState({visible: true});
     };
-    
+
     // 获取列表
     getList = (i18nKey = '') => {
         this.setState({loading: true});
         axios({
-            method: 'GET' ,
-            url: '/intl/list' ,
+            method: 'GET',
+            url: '/intl/list',
             params: {
                 i18nKey
             }
         }).then(res => {
             this.setState({loading: false});
-            const dataList = res.data;
+            const dataList = res.data.data;
             this.setState({dataList});
         });
     };
-    
+
     // 关闭模态框
     onClose = () => {
-        this.setState({visible: false});
+        this.setState({visible: false, rowData: null});
     };
-    
+
     // 保存
     onSave = (data) => {
         const method = data._id ? 'PUT' : 'POST';
         const url = data._id ? '/intl/updata' : '/intl/add';
         axios({
-            method ,
-            url ,
+            method,
+            url,
             data: {
-                appId: 'ccp' ,
-                appName: 'eCooperate™' ,
+                appId: 'ccp',
+                appName: 'eCooperate™',
                 ...data
             }
-        }).then(() => {
-            this.getList();
-            this.onClose();
+        }).then(res => {
+            if(res.data.success){
+                this.getList();
+                this.onClose();
+            }else{
+                message.error(res.data.errorMessage)
+            }
         });
     };
-    
+
     // 删除
     onDelete = (id) => {
         axios({
-            method: 'DELETE' ,
-            url: `/intl/delete/` ,
+            method: 'DELETE',
+            url: `/intl/delete/`,
             params: {
                 _id: id
             }
@@ -127,22 +131,37 @@ class Index extends Component {
             this.onClose();
         });
     };
-    
+
     // 编辑
     onEdit = (rowData) => {
-        this.setState({rowData: rowData , visible: true});
+        this.setState({rowData: rowData, visible: true});
     };
-    
+
     render() {
-        const {dataList , loading , visible , rowData} = this.state;
-        
+        const {dataList, loading, visible, rowData} = this.state;
+        console.log('dataList',dataList)
+        let that = this;
+        const props = {
+            name: 'file',
+            action: '/intl/import',
+            showUploadList: false,
+            onChange(info) {
+                if (info.file.status === 'done') {
+                    message.success("上传成功");
+                    that.getList();
+                } else if (info.file.status === 'error') {
+                    message.success("上传失败");
+                }
+            },
+        };
+
         return (
             <React.Fragment>
                 <div style={{
-                    width: '80%' ,
-                    margin: '0 auto' ,
-                    background: '#fff' ,
-                    padding: 10 ,
+                    width: '80%',
+                    margin: '0 auto',
+                    background: '#fff',
+                    padding: 10,
                     // border: '1px solid #ebedf0',
                     // boxShadow:'rgb(241, 241, 241) 1px 2px 3px 4px'
                 }}>
@@ -156,19 +175,21 @@ class Index extends Component {
                         <Col span={8} offset={6} style={{textAlign: 'right'}}>
                             <Button type="primary" icon="plus" style={{marginRight: 10}}
                                     onClick={this.onAdd}>新增</Button>
-                            <Button icon="upload" style={{marginRight: 10}}>导入</Button>
+                            <Upload {...props}>
+                                <Button icon="upload" style={{marginRight: 10}}>导入</Button>
+                            </Upload>
                             <Button icon="download">导出</Button>
                         </Col>
                     </Row>
-                    <Table loading={loading} size="middle" rowKey="_id" columns={this.colums} dataSource={dataList} />
-                    <AddOrEditModal rowData={rowData} visible={visible} onClose={this.onClose} onSave={this.onSave} />
+                    <Table loading={loading} size="middle" rowKey="_id" columns={this.colums} dataSource={dataList}/>
+                    <AddOrEditModal rowData={rowData} visible={visible} onClose={this.onClose} onSave={this.onSave}/>
                 </div>
                 <div style={{
-                    textAlign: 'center' ,
-                    paddingTop: 10 ,
+                    textAlign: 'center',
+                    paddingTop: 10,
                     paddingBottom: 10
                 }}>
-                    TaiMei &copy; 2019 Created by snowden
+                    TaiMei &copy; 2019
                 </div>
             </React.Fragment>
         );
